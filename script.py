8f00946b0d56620b32d0bd7e6379d9eca7ce0b88
Tushar Aggarwal <tushar1997@gmail.com>
2020-04-07 00:49:20 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+from flask import Flask
+
+app = Flask(__name__)
+URL = 'https://www.mohfw.gov.in'
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+
+@app.route("/")
+def hello():
+  sum = 0
+  for div in soup.find_all('div', { "class" : "site-stats-count"}):
+    for li in div.find_all('li'):
+      for value in li.find_all('strong'):
+        sum += int(value.text)
+        
+    return str(sum)
+
+
+if __name__ == '__main__':
+    app.run()
