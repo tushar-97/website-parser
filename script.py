a8a28ef4ace2bb9a54ae37e15dfd36c4a7bd6386
Tushar Aggarwal <tushar1997@gmail.com>
2020-04-07 17:14:54 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,23 +1,48 @@
 import requests
 from bs4 import BeautifulSoup
 from flask import Flask
+from flask import jsonify
 
 app = Flask(__name__)
+
 URL = 'https://www.mohfw.gov.in'
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, 'html.parser')
 
 
 @app.route("/")
-def hello():
+def details():
+  sum = 0
+  data = []
+  for div in soup.find_all('div', { "class" : "site-stats-count"}):
+    for li in div.find_all('li'):
+      for value in li.find_all('strong'):
+        data.append(value.text)
+        sum += int(value.text)
+        
+    data.append(sum)
+
+    json = {}
+    json['Active Cases'] = data[0]
+    json['Cured / Discharged'] = data[1]
+    json['Deaths'] = data[2]
+    json['Migrated'] = data[3]
+    json['Total'] = str(data[4])
+
+    return jsonify(json)
+
+
+@app.route("/total")
+def total():
   sum = 0
   for div in soup.find_all('div', { "class" : "site-stats-count"}):
     for li in div.find_all('li'):
       for value in li.find_all('strong'):
         sum += int(value.text)
-        
+
     return str(sum)
 
 
 if __name__ == '__main__':
+    app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
     app.run(host='0.0.0.0',port=80)
